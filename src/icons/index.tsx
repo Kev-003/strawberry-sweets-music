f5f41b035deb0fefeaae925fffe90a002e154d57
Kevern Angeles <kevern920@gmail.com>
2026-06-10 12:02:09 +0800

    aaaaaaaaaa

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,163 +1,189 @@
-export const PlusIcon = () => (
-  <img src="/icons/plus.svg" alt="" className="w-6 h-6" />
+import React from "react";
+
+interface IconProps {
+  className?: string;
+}
+
+export const PlusIcon = ({ className }: IconProps) => (
+  <img src="/icons/plus.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const CloseIcon = () => (
-  <img src="/icons/close.svg" alt="" className="w-6 h-6" />
+export const CloseIcon = ({ className }: IconProps) => (
+  <img src="/icons/close.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const BoxIcon = () => (
-  <img src="/icons/box.svg" alt="" className="w-6 h-6" />
+export const BoxIcon = ({ className }: IconProps) => (
+  <img src="/icons/box.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const CheckCircleIcon = () => (
-  <img src="/icons/check-circle.svg" alt="" className="w-6 h-6" />
+export const CheckCircleIcon = ({ className }: IconProps) => (
+  <img
+    src="/icons/check-circle.svg"
+    alt=""
+    className={className ?? "w-6 h-6"}
+  />
 );
-export const AlertIcon = () => (
-  <img src="/icons/alert.svg" alt="" className="w-6 h-6" />
+export const AlertIcon = ({ className }: IconProps) => (
+  <img src="/icons/alert.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const InfoIcon = () => (
-  <img src="/icons/info.svg" alt="" className="w-6 h-6" />
+export const InfoIcon = ({ className }: IconProps) => (
+  <img src="/icons/info.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ErrorIcon = () => (
-  <img src="/icons/info-hexa.svg" alt="" className="w-6 h-6" />
+export const ErrorIcon = ({ className }: IconProps) => (
+  <img src="/icons/info-hexa.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const BoltIcon = () => (
-  <img src="/icons/bolt.svg" alt="" className="w-6 h-6" />
+export const BoltIcon = ({ className }: IconProps) => (
+  <img src="/icons/bolt.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ArrowUpIcon = () => (
-  <img src="/icons/arrow-up.svg" alt="" className="w-6 h-6" />
+export const ArrowUpIcon = ({ className }: IconProps) => (
+  <img src="/icons/arrow-up.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ArrowDownIcon = () => (
-  <img src="/icons/arrow-down.svg" alt="" className="w-6 h-6" />
+export const ArrowDownIcon = ({ className }: IconProps) => (
+  <img src="/icons/arrow-down.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const FolderIcon = () => (
-  <img src="/icons/folder.svg" alt="" className="w-6 h-6" />
+export const FolderIcon = ({ className }: IconProps) => (
+  <img src="/icons/folder.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const VideoIcon = () => (
-  <img src="/icons/videos.svg" alt="" className="w-6 h-6" />
+export const VideoIcon = ({ className }: IconProps) => (
+  <img src="/icons/videos.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const AudioIcon = () => (
-  <img src="/icons/audio.svg" alt="" className="w-6 h-6" />
+export const AudioIcon = ({ className }: IconProps) => (
+  <img src="/icons/audio.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const GridIcon = () => (
-  <img src="/icons/grid.svg" alt="" className="w-6 h-6" />
+export const GridIcon = ({ className }: IconProps) => (
+  <img src="/icons/grid.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const FileIcon = () => (
-  <img src="/icons/file.svg" alt="" className="w-6 h-6" />
+export const FileIcon = ({ className }: IconProps) => (
+  <img src="/icons/file.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const DownloadIcon = () => (
-  <img src="/icons/download.svg" alt="" className="w-6 h-6" />
+export const DownloadIcon = ({ className }: IconProps) => (
+  <img src="/icons/download.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ArrowRightIcon = () => (
-  <img src="/icons/arrow-right.svg" alt="" className="w-6 h-6" />
+export const ArrowRightIcon = ({ className }: IconProps) => (
+  <img src="/icons/arrow-right.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const GroupIcon = () => (
-  <img src="/icons/group.svg" alt="" className="w-6 h-6" />
+export const GroupIcon = ({ className }: IconProps) => (
+  <img src="/icons/group.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const BoxIconLine = () => (
-  <img src="/icons/box-line.svg" alt="" className="w-6 h-6" />
+export const BoxIconLine = ({ className }: IconProps) => (
+  <img src="/icons/box-line.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ShootingStarIcon = () => (
-  <img src="/icons/shooting-star.svg" alt="" className="w-6 h-6" />
+export const ShootingStarIcon = ({ className }: IconProps) => (
+  <img
+    src="/icons/shooting-star.svg"
+    alt=""
+    className={className ?? "w-6 h-6"}
+  />
 );
-export const DollarLineIcon = () => (
-  <img src="/icons/dollar-line.svg" alt="" className="w-6 h-6" />
+export const DollarLineIcon = ({ className }: IconProps) => (
+  <img src="/icons/dollar-line.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const TrashBinIcon = () => (
-  <img src="/icons/trash.svg" alt="" className="w-6 h-6" />
+export const TrashBinIcon = ({ className }: IconProps) => (
+  <img src="/icons/trash.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const AngleUpIcon = () => (
-  <img src="/icons/angle-up.svg" alt="" className="w-6 h-6" />
+export const AngleUpIcon = ({ className }: IconProps) => (
+  <img src="/icons/angle-up.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const AngleDownIcon = () => (
-  <img src="/icons/angle-down.svg" alt="" className="w-6 h-6" />
+export const AngleDownIcon = ({ className }: IconProps) => (
+  <img src="/icons/angle-down.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const PencilIcon = () => (
-  <img src="/icons/pencil.svg" alt="" className="w-6 h-6" />
+export const PencilIcon = ({ className }: IconProps) => (
+  <img src="/icons/pencil.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const CheckLineIcon = () => (
-  <img src="/icons/check-line.svg" alt="" className="w-6 h-6" />
+export const CheckLineIcon = ({ className }: IconProps) => (
+  <img src="/icons/check-line.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const CloseLineIcon = () => (
-  <img src="/icons/close-line.svg" alt="" className="w-6 h-6" />
+export const CloseLineIcon = ({ className }: IconProps) => (
+  <img src="/icons/close-line.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ChevronDownIcon = ({ className }: { className?: string }) => (
+export const ChevronDownIcon = ({ className }: IconProps) => (
   <img
     src="/icons/chevron-down.svg"
     alt=""
     className={className ?? "w-5 h-5"}
   />
 );
-export const ChevronUpIcon = () => (
-  <img src="/icons/chevron-up.svg" alt="" className="w-5 h-5" />
+export const ChevronUpIcon = ({ className }: IconProps) => (
+  <img src="/icons/chevron-up.svg" alt="" className={className ?? "w-5 h-5"} />
 );
-export const ChevronLeftIcon = () => (
-  <img src="/icons/chevron-left.svg" alt="" className="w-5 h-5" />
+export const ChevronLeftIcon = ({ className }: IconProps) => (
+  <img
+    src="/icons/chevron-left.svg"
+    alt=""
+    className={className ?? "w-5 h-5"}
+  />
 );
-export const PaperPlaneIcon = () => (
-  <img src="/icons/paper-plane.svg" alt="" className="w-6 h-6" />
+export const PaperPlaneIcon = ({ className }: IconProps) => (
+  <img src="/icons/paper-plane.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const LockIcon = () => (
-  <img src="/icons/lock.svg" alt="" className="w-6 h-6" />
+export const LockIcon = ({ className }: IconProps) => (
+  <img src="/icons/lock.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const EnvelopeIcon = () => (
-  <img src="/icons/envelope.svg" alt="" className="w-6 h-6" />
+export const EnvelopeIcon = ({ className }: IconProps) => (
+  <img src="/icons/envelope.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const UserIcon = () => (
-  <img src="/icons/user-line.svg" alt="" className="w-6 h-6" />
+export const UserIcon = ({ className }: IconProps) => (
+  <img src="/icons/user-line.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const CalenderIcon = () => (
-  <img src="/icons/calender-line.svg" alt="" className="w-6 h-6" />
+export const CalenderIcon = ({ className }: IconProps) => (
+  <img
+    src="/icons/calender-line.svg"
+    alt=""
+    className={className ?? "w-6 h-6"}
+  />
 );
-export const EyeIcon = () => (
-  <img src="/icons/eye.svg" alt="" className="w-6 h-6" />
+export const EyeIcon = ({ className }: IconProps) => (
+  <img src="/icons/eye.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const EyeCloseIcon = () => (
-  <img src="/icons/eye-close.svg" alt="" className="w-6 h-6" />
+export const EyeCloseIcon = ({ className }: IconProps) => (
+  <img src="/icons/eye-close.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const TimeIcon = () => (
-  <img src="/icons/time.svg" alt="" className="w-6 h-6" />
+export const TimeIcon = ({ className }: IconProps) => (
+  <img src="/icons/time.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const CopyIcon = () => (
-  <img src="/icons/copy.svg" alt="" className="w-6 h-6" />
+export const CopyIcon = ({ className }: IconProps) => (
+  <img src="/icons/copy.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const UserCircleIcon = () => (
-  <img src="/icons/user-circle.svg" alt="" className="w-6 h-6" />
+export const UserCircleIcon = ({ className }: IconProps) => (
+  <img src="/icons/user-circle.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const TaskIcon = () => (
-  <img src="/icons/task-icon.svg" alt="" className="w-6 h-6" />
+export const TaskIcon = ({ className }: IconProps) => (
+  <img src="/icons/task-icon.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const ListIcon = () => (
-  <img src="/icons/list.svg" alt="" className="w-6 h-6" />
+export const ListIcon = ({ className }: IconProps) => (
+  <img src="/icons/list.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const TableIcon = () => (
-  <img src="/icons/table.svg" alt="" className="w-6 h-6" />
+export const TableIcon = ({ className }: IconProps) => (
+  <img src="/icons/table.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const PageIcon = () => (
-  <img src="/icons/page.svg" alt="" className="w-6 h-6" />
+export const PageIcon = ({ className }: IconProps) => (
+  <img src="/icons/page.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const PieChartIcon = () => (
-  <img src="/icons/pie-chart.svg" alt="" className="w-6 h-6" />
+export const PieChartIcon = ({ className }: IconProps) => (
+  <img src="/icons/pie-chart.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const BoxCubeIcon = () => (
-  <img src="/icons/box-cube.svg" alt="" className="w-6 h-6" />
+export const BoxCubeIcon = ({ className }: IconProps) => (
+  <img src="/icons/box-cube.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const PlugInIcon = () => (
-  <img src="/icons/plug-in.svg" alt="" className="w-6 h-6" />
+export const PlugInIcon = ({ className }: IconProps) => (
+  <img src="/icons/plug-in.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const DocsIcon = () => (
-  <img src="/icons/docs.svg" alt="" className="w-6 h-6" />
+export const DocsIcon = ({ className }: IconProps) => (
+  <img src="/icons/docs.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const MailIcon = () => (
-  <img src="/icons/mail-line.svg" alt="" className="w-6 h-6" />
+export const MailIcon = ({ className }: IconProps) => (
+  <img src="/icons/mail-line.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const HorizontaLDots = () => (
-  <img src="/icons/horizontal-dots.svg" alt="" className="w-6 h-6" />
+export const HorizontaLDots = ({ className }: IconProps) => (
+  <img
+    src="/icons/horizontal-dots.svg"
+    alt=""
+    className={className ?? "w-6 h-6"}
+  />
 );
-export const ChatIcon = () => (
-  <img src="/icons/chat.svg" alt="" className="w-6 h-6" />
+export const ChatIcon = ({ className }: IconProps) => (
+  <img src="/icons/chat.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const MoreDotIcon = () => (
-  <img src="/icons/more-dot.svg" alt="" className="w-6 h-6" />
+export const MoreDotIcon = ({ className }: IconProps) => (
+  <img src="/icons/more-dot.svg" alt="" className={className ?? "w-6 h-6"} />
 );
-export const BellIcon = () => (
-  <img src="/icons/bell.svg" alt="" className="w-6 h-6" />
+export const BellIcon = ({ className }: IconProps) => (
+  <img src="/icons/bell.svg" alt="" className={className ?? "w-6 h-6"} />
 );
